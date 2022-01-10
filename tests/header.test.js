f9fce106bb641880e6327d7a9684ada74af4f16d
David Kolosowski <david.kolosowski@gmail.com>
2022-01-10 20:57:16 +0000

    Fix tests

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,5 @@
 import { h } from 'preact';
+import { Link } from 'preact-router/match';
 import Header from '../src/components/header';
 // See: https://github.com/preactjs/enzyme-adapter-preact-pure
 import { shallow } from 'enzyme';
@@ -7,6 +8,6 @@ describe('Initial Test of the Header', () => {
 	test('Header renders 3 nav items', () => {
 		const context = shallow(<Header />);
 		expect(context.find('h1').text()).toBe('Preact App');
-		expect(context.find('Link').length).toBe(3);
+		expect(context.find(Link).length).toBe(3);
 	});
 });
