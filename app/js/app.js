9bbc66812eea8fe8eac350a55d5f46d19294bb35
David Kolosowski <david.kolosowski@gmail.com>
2022-01-10 10:18:06 +0000

    Get rid of old code to start anew

--- app/js/app.js
+++
@@ -1,7 +0,0 @@
-angular.module('ngnix-config', ['ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ngnix-config.directives',
-                                   'ngnix-config.filters', 'ngnix-config.system', 'ui.layout', 'angularTreeview']);
-								   
-								   
-angular.module('ngnix-config.system', []);
-angular.module('ngnix-config.directives', []);
-angular.module('ngnix-config.filters', []);
