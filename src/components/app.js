4144496cd7c6b04e8ffd4232ca59b532e3e14f63
David Kolosowski <david.kolosowski@gmail.com>
2022-01-11 11:12:39 +0000

    Fix links on github pages

--- src/components/app.js
+++ src/components/app.js
@@ -6,11 +6,13 @@ import Header from './header';
 // Code-splitting is automated for `routes` directory
 import Home from '../routes/home';
 import Profile from '../routes/profile';
+import { createHashHistory } from 'history';
+
 
 const App = () => (
 	<div id="app">
 		<Header />
-		<Router>
+		<Router history={ createHashHistory() }>
 			<Home path="/" />
 			<Profile path="/profile/" user="me" />
 			<Profile path="/profile/:user" />
