d8dc74ac87a63a83c05747520ce4a7e1ddc6087e
David Kolosowski <david.kolosowski@gmail.com>
2022-01-21 15:32:20 +0000

    Fix CI nag

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,7 +1,7 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Fragment } from 'react'
 import { Disclosure } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
 
 const navigation = [
   { name: 'Create New', href: '#', current: true },
